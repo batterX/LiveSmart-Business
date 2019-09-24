f578498072934d35e82a7c924a3542c521ea6f6b
batterX <ivan@visionups.com>
2019-09-24 13:11:36 +0200

    v19.8.1

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # updater.sh
 
-sudo rm /home/pi/DataLogger
-sudo cp /home/pi/livesmart-business/DataLogger /home/pi
-sudo chmod 777 /home/pi/DataLogger
+#sudo rm /home/pi/DataLogger
+#sudo cp /home/pi/livesmart-business/DataLogger /home/pi
+#sudo chmod 777 /home/pi/DataLogger
 
-#sudo rm /home/pi/CloudStream
-#sudo cp /home/pi/livesmart-business/CloudStream /home/pi
-#sudo chmod 777 /home/pi/CloudStream
+sudo rm /home/pi/CloudStream
+sudo cp /home/pi/livesmart-business/CloudStream /home/pi
+sudo chmod 777 /home/pi/CloudStream
 
 #sudo rm /home/pi/GetApikey
 #sudo cp /home/pi/livesmart-business/GetApikey /home/pi
