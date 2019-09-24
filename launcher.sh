ef4599a7d8208449d62dc50493f6cf37304559d0
batterX <ivan@visionups.com>
2019-09-24 11:04:18 +0200

    v19.8.1

--- launcher.sh
+++ launcher.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # launcher.sh
 
-if ! pgrep -x "DataLogger" > /dev/null
-then
-	cd /
-	cd home/pi
-	sudo ./DataLogger
-	cd /
-fi
+#if ! pgrep -x "DataLogger" > /dev/null
+#then
+#	cd /
+#	cd home/pi
+#	sudo ./DataLogger
+#	cd /
+#fi
 
 if ! pgrep -x "CloudStream" > /dev/null
 then
--- update.sh
+++ update.sh
@@ -5,13 +5,13 @@ sudo rm /home/pi/DataLogger
 sudo cp /home/pi/livesmart-business/DataLogger /home/pi
 sudo chmod 777 /home/pi/DataLogger
 
-sudo rm /home/pi/CloudStream
-sudo cp /home/pi/livesmart-business/CloudStream /home/pi
-sudo chmod 777 /home/pi/CloudStream
+#sudo rm /home/pi/CloudStream
+#sudo cp /home/pi/livesmart-business/CloudStream /home/pi
+#sudo chmod 777 /home/pi/CloudStream
 
-sudo rm /home/pi/GetApikey
-sudo cp /home/pi/livesmart-business/GetApikey /home/pi
-sudo chmod 777 /home/pi/GetApikey
+#sudo rm /home/pi/GetApikey
+#sudo cp /home/pi/livesmart-business/GetApikey /home/pi
+#sudo chmod 777 /home/pi/GetApikey
 
 sudo cp /home/pi/livesmart-business/launcher.sh /home/pi
 sudo chmod 777 /home/pi/launcher.sh
